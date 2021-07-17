5e09ceae6ebe982b77abc98e351412d0173217e9
joonhee0908 <81891143+joonhee0908@users.noreply.github.com>
2021-07-17 16:29:05 +0900

    반환값 표현 방식 변경
    반환값을 [{key1:value1}, {key2:value2}, {key3:value3}] 와 같이 표현되도록 수정하였습니다.

--- form_get.js
+++ form_get.js
@@ -2,38 +2,44 @@ function get() {
     var url = location.href;
     var keyarr = [];
     var valuearr = [];
-    if(url.indexOf("?") != -1) {
+    if(url.indexOf("?") !== -1) {
         url =  url.slice(url.indexOf("?") + 1);
         var index = 0;
         var arrindex = 0;
-        if(url.length != 0) {
+        if(url.length !== 0) {
             while(true) {
                 while(true) {
-                    if(url.charAt(index) != "=") {
-                        if(keyarr[arrindex] != undefined){keyarr[arrindex] += url.charAt(index);} else{keyarr[arrindex] = url.charAt(index);}
-                        index++;
+                    if(url.charAt(index) !== "=") {
+                        if(keyarr[arrindex] !== undefined){keyarr[arrindex] += url.charAt(index);} else{keyarr[arrindex] = url.charAt(index);}
+                        index+= 1;
                     } else {
                         break;
                     }
                 }
                 while(true) {
-                    if(url.charAt(index) != "&" || url.charAt(index) != "") {
-                        index++;
-                        if(url.charAt(index) == "" || url.charAt(index) == "&") {
+                    if(url.charAt(index) !== "&" || url.charAt(index) !== "") {
+                        index+= 1;
+                        if(url.charAt(index) === "" || url.charAt(index) === "&") {
                             break;
                         }
-                        if(valuearr[arrindex] != undefined){valuearr[arrindex] += url.charAt(index);} else{valuearr[arrindex] = url.charAt(index);}
+                        if(valuearr[arrindex] !== undefined){valuearr[arrindex] += url.charAt(index);} else{valuearr[arrindex] = url.charAt(index);}
                     } else {
                         break;
                     }
                 }
-                index++;
-                arrindex++;
+                index+= 1;
+                arrindex+= 1;
                 if(index + 1 > url.length) {
                     break;
                 }
             }
-            var returnvalue = [keyarr, valuearr];
+            var returnvalue = [];
+            for (let i =0;i<arrindex;i+=1) {
+                var key = keyarr[i];
+                var value = valuearr[i]
+                returnvalue[i] = {}
+                returnvalue[i]  [key+''] = value;
+            }
             return returnvalue;
         } else {
             return undefined;
