7097d71de82a88ea01117466833f7d7a368611ce
devjh908 <81891143+devjh908@users.noreply.github.com>
2023-02-07 17:54:52 +0900

    Update form_get.js

--- form_get.js
+++ form_get.js
@@ -1,42 +1,42 @@
 function get() {
-    var url = location.href;
-    var keyarr = [];
-    var valuearr = [];
+    let url = location.href;
+    let karr = [];
+    let varr = [];
     if(url.indexOf("?") !== -1) {
         url =  url.slice(url.indexOf("?") + 1);
-        var index = 0;
-        var arrindex = 0;
+        let ind = 0;
+        let arrind = 0;
         if(url.length !== 0) {
             while(true) {
                 while(true) {
-                    if(url.charAt(index) !== "=") {
-                        if(keyarr[arrindex] !== undefined){keyarr[arrindex] += url.charAt(index);} else{keyarr[arrindex] = url.charAt(index);}
-                        index+= 1;
+                    if(url.charAt(ind) !== "=") {
+                        if(karr[arrind] !== undefined){karr[arrind] += url.charAt(ind);} else{karr[arrinx] = url.charAt(ind);}
+                        ind+= 1;
                     } else {
                         break;
                     }
                 }
                 while(true) {
-                    if(url.charAt(index) !== "&" || url.charAt(index) !== "") {
-                        index+= 1;
-                        if(url.charAt(index) === "" || url.charAt(index) === "&") {
+                    if(url.charAt(ind) !== "&" || url.charAt(ind) !== "") {
+                        ind+= 1;
+                        if(url.charAt(ind) === "" || url.charAt(ind) === "&") {
                             break;
                         }
-                        if(valuearr[arrindex] !== undefined){valuearr[arrindex] += url.charAt(index);} else{valuearr[arrindex] = url.charAt(index);}
+                        if(varr[arrind] !== undefined){varr[arrind] += url.charAt(ind);} else{varr[arrind] = url.charAt(ind);}
                     } else {
                         break;
                     }
                 }
-                index+= 1;
-                arrindex+= 1;
-                if(index + 1 > url.length) {
+                ind+= 1;
+                arrind+= 1;
+                if(ind + 1 > url.length) {
                     break;
                 }
             }
             var returnvalue = {};
             for (let i = 0; i<arrindex;i+=1) {
-                var key = keyarr[i];
-                var value = valuearr[i];
+                let key = karr[i];
+                let value = varr[i];
                 returnvalue[key+''] = value;
             }
             return returnvalue;
